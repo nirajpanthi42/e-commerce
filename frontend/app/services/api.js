d61b8e9fb2917dae761e328e30472de7770f2b8d
Niraj Panthi <nirajpanthi42@gmail.com>
2026-07-16 23:05:23 +0545

    update

--- frontend/app/services/api.js
+++ frontend/app/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL:"https://e-commerce-3-23ca.onrender.com/api"
 });
 
 // Attach JWT token automatically
